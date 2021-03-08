e15661198d654d0263225b222d5e6876e20d9dd9
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-08 07:03:51 -0500

    javascript object attached to $scope

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,5 +1,7 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("myController", function ($scope) {
+  var employee = { firstname: 'Sam', lastname: 'Smith', gender: 'Male'};
+  $scope.employee = employee;
   $scope.title = "AngularJS Tutorial"
 });
