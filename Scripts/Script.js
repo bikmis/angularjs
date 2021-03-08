3809839c8d422e03b8444d54a6a85cb8e322e763
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-08 08:06:41 -0500

    image

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -2,8 +2,15 @@ var myModule = angular.module("myModule", [])
                       .controller('myController',  function ($scope) {
                             var employee = { firstname: 'Sam', 
                                              lastname: 'Smith', 
-                                             gender: 'Male'};
+                                             gender: 'Male'
+                                            };
                             $scope.employee = employee;
-                            $scope.title = "AngularJS Tutorial"
+                            $scope.title = "AngularJS Tutorial";
+                            var country = {
+                              name: 'USA',
+                              capital: 'Washington DC',
+                              flag: '/images/usaFlag.png'
+                            }
+                            $scope.country = country;
                       });
 
