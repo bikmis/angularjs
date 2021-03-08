18f2a053a8d3d71a809160b0b9033ec358080743
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-08 07:50:35 -0500

    method chaining

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,7 +1,9 @@
-var myModule = angular.module("myModule", []);
+var myModule = angular.module("myModule", [])
+                      .controller('myController',  function ($scope) {
+                            var employee = { firstname: 'Sam', 
+                                             lastname: 'Smith', 
+                                             gender: 'Male'};
+                            $scope.employee = employee;
+                            $scope.title = "AngularJS Tutorial"
+                      });
 
-myModule.controller("myController", function ($scope) {
-  var employee = { firstname: 'Sam', lastname: 'Smith', gender: 'Male'};
-  $scope.employee = employee;
-  $scope.title = "AngularJS Tutorial"
-});
