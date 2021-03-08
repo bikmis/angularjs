b89053e7ba87af4f760ab71952586520a7ba361d
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-07 21:43:55 -0500

    controller, module

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,1 +1,7 @@
-var myApp = angular.module()
+/// <reference path="angularjs.min.js"/>
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+  $scope.title = "AngularJS Tutorial"
+});
