e947abfb52c47eb0a726d786011a8d0bf85899fa
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-18 12:08:32 -0400

    sorting

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -37,10 +37,16 @@ myModule.controller('myFilter', function ($scope) {
   var employees = [
     { name: 'Jack', dateOfBirth: new Date('01/15/1962'), gender: 'Male', salary: 5000487.357 },
     { name: 'Mark', dateOfBirth: new Date('05/19/1975'), gender: 'Male', salary: 478012.68 },
-    { name: 'Anita', dateOfBirth: new Date('09/29/1981'), gender: 'Female',salary: 92490.46 }
+    { name: 'Anita', dateOfBirth: new Date('09/29/1981'), gender: 'Female', salary: 92490.46 }
   ]
 
   $scope.employees = employees;
   $scope.numberOfRows = 3;
-  $scope.sortColumn = '-salary'
+  $scope.sortColumn = 'name'
+  $scope.sortColumnOrder = '+';
+
+  $scope.executeSortColumn = function (column) {
+    $scope.sortColumnOrder = $scope.sortColumnOrder === '+' ? '-' : '+'
+    $scope.sortColumn = column;
+  }
 })
