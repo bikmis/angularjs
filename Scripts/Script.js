8b2504d6a72d23b147bf21141ed583fc1109ca48
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-20 13:36:26 -0400

    filter separated

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -64,15 +64,3 @@ myModule.controller('myFilter', function ($scope) {
   }
 })
 
-myModule.filter('salary', function () {
-  return function (salary) {
-    switch (true) {
-      case (salary <= 100000):
-        return 'Too Little';
-      case (salary > 100000 && salary <= 500000):
-        return 'Moderate Amount';
-      case salary > 500000:
-        return 'Too Much';
-    }
-  }
-})
---
+++ Scripts/filter.js
@@ -0,0 +1,12 @@
+myModule.filter('salary', function () {
+  return function (salary) {
+    switch (true) {
+      case (salary <= 100000):
+        return 'Too Little';
+      case (salary > 100000 && salary <= 500000):
+        return 'Moderate Amount';
+      case salary > 500000:
+        return 'Too Much';
+    }
+  }
+})
