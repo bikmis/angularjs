81ebcde0a21c2d55df74dc839962c0e2568cd414
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-25 10:08:52 -0400

    setting title on route change

--- Scripts/appModule.js
+++ Scripts/appModule.js
@@ -3,15 +3,18 @@ var appModule = angular.module('appModule', ['ngRoute'])
     $routeProvider
       .when('/home', {
         templateUrl: 'templates/home.html',
-        controller: 'homeController'
+        controller: 'homeController', 
+        title:'Home'
       })
       .when('/employee', {
         templateUrl: 'templates/employee.html',
-        controller: 'employeeController'
+        controller: 'employeeController',
+        title: 'Employee'
       })
       .when('/filterSortingExample', {
         templateUrl: 'templates/filterSortingExample.html',
-        controller: 'employee_2_Controller'
+        controller: 'employee_2_Controller',
+        title: 'Filter & Sorting'
       })
       .otherwise({
         redirectTo: '/home' //default route, http://127.0.0.1:5500/home and http://127.0.0.1:5500 open the home page(/home)
@@ -19,3 +22,8 @@ var appModule = angular.module('appModule', ['ngRoute'])
     $locationProvider.html5Mode(true);
   })
   
+  appModule.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        document.title = $route.current.title;
+    });
+  }]);
