84eb8b9a67ef3aedca138898744b2fa73e166456
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-26 12:07:11 -0400

    case insensitive routes

--- Scripts/appModule.js
+++ Scripts/appModule.js
@@ -1,41 +1,36 @@
 var appModule = angular.module('appModule', ['ngRoute'])
   .config(function ($routeProvider, $locationProvider) {
+    $routeProvider.caseInsensitiveMatch = true;
     $routeProvider
       .when('/home', {
         templateUrl: 'templates/home.html',
         controller: 'homeController', 
-        title:'Home',
-        caseInsensitiveMatch: true
+        title:'Home'
       })
       .when('/employee', {
         templateUrl: 'templates/employee.html',
         controller: 'employeeController',
-        title: 'Employee',
-        caseInsensitiveMatch: true
+        title: 'Employee'
       })
       .when('/filterSortingExample', {
         templateUrl: 'templates/filterSortingExample.html',
         controller: 'employee_2_Controller',
-        title: 'Filter & Sorting',
-        caseInsensitiveMatch: true
+        title: 'Filter & Sorting'
       })
-      .when('/students', {
+      .when('/Students', {
         templateUrl: 'templates/student/studentList.html',
         controller: 'studentListController',
-        title: 'List of Students',
-        caseInsensitiveMatch: true
+        title: 'List of Students'
       })
       .when('/student/:id', {
         templateUrl: 'templates/student/detailStudent.html',
         controller: 'detailStudentController',
-        title: 'Student Detail',
-        caseInsensitiveMatch: true
+        title: 'Student Detail'
       })
       .when('/add-student-form', {
         templateUrl: 'templates/student/addStudentForm.html',
         controller: 'addStudentFormController',
-        title: 'Add Student Form',
-        caseInsensitiveMatch: true
+        title: 'Add Student Form'
       })
       .otherwise({
         redirectTo: '/home' //default route, http://127.0.0.1:5500/home and http://127.0.0.1:5500 open the home page(/home)
