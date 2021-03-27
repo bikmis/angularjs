87870e81c806bec6b872d72e122490669ec5dbe9
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-03-27 17:47:44 -0400

    route change

--- Scripts/student/studentListController.js
+++ Scripts/student/studentListController.js
@@ -1,7 +1,7 @@
 appModule.controller('studentListController', function ($rootScope, $scope, $location, $routeParams, $route) {
   $rootScope.footer = 'This is a value of a property "footer" on $rootScope object in "studentListController" and it is available in all controllers. In the index.html this file should be referenced above other script files';
 
-  init = function(){
+  init = function () {
     saveStudent();
   };
 
@@ -13,33 +13,45 @@ appModule.controller('studentListController', function ($rootScope, $scope, $loc
   ]
 
   $scope.students = students;
-  $scope.detailStudent = function(id){
+  $scope.detailStudent = function (id) {
     var student = students.find(s => s.id === id);
-    $location.path('student/'+id).search('student', student);
+    $location.path('student/' + id).search('student', student);
   }
 
-  $scope.deleteStudent = function(id){
+  $scope.deleteStudent = function (id) {
     var student = students.find(s => s.id === id);
     var studentIndex = students.indexOf(student);
     students.splice(studentIndex, 1);
     $scope.students = students;
   }
 
-  $scope.addStudentForm = function(){
+  $scope.addStudentForm = function () {
     $location.path('add-student-form');
   }
 
-  saveStudent = function(){
-    if($routeParams.student != undefined){
+  saveStudent = function () {
+    if ($routeParams.student != undefined) {
       var student = $routeParams.student;
       $scope.students.push(student);
     }
   };
 
-  $scope.reload = function(){
+  $scope.reload = function () {
     $route.reload();
   }
 
+  // We can use $routeChangeStart or $locationChangeStart to capture the event when we navigate away. 
+  /*
+  $scope.$on('$routeChangeStart', function(event, next, current){
+    if(!confirm('Are you sure you want to navigate away to ' + next.$$route.originalPath+'?'))
+    event.preventDefault();
+  })
+  */
+  $scope.$on('$locationChangeStart', function (event, next, current) {
+    if (!confirm('Are you sure you want to navigate away to ' + next + ' from ' + current + '?'))
+      event.preventDefault();
+  })
+
   init();
-  
+
 })
