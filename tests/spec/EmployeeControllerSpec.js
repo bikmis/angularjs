9fcae03360828b4d23b66a7127c3382aefe6cab9
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-04-13 15:08:43 -0400

    code refactored

--- tests/spec/EmployeeControllerSpec.js
+++ tests/spec/EmployeeControllerSpec.js
@@ -2,26 +2,26 @@ describe('employeeController', function () {
 
   beforeEach(module("appModule"));
 
-  var $controller, $rootScope, helloService;
+  var $controller, $rootScope, helloService, $scope;
 
-  //helloService.js needs to be referenced in SpecRunner.html
+  //appModule.js, employeeController.js, and helloService.js need to be referenced in SpecRunner.html
   beforeEach(inject(function (_$controller_, _$rootScope_, _helloService_) {
     $controller = _$controller_;
     $rootScope = _$rootScope_;
     helloService = _helloService_;
   }))
 
+  beforeEach(function(){
+    $scope = $rootScope.$new();
+    $controller('employeeController', { $scope: $scope });
+  })
 
   it('sum', function () {
-    var $scope = $rootScope.$new();
-    $controller('employeeController', { $scope: $scope });
     var sum = $scope.add(2, 3);
     expect(5).toEqual(sum);
   })
 
   it('say hello', function () {
-    var $scope = $rootScope.$new();
-    $controller('employeeController', { $scope: $scope });
     var greeting = $scope.sayHello('Jack')
     expect('Hello Jack').toEqual(greeting);
   })
